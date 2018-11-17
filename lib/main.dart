bc2122fbac394c49cb7dc710568bdfa9e6b8b90e
Evgeny Shevchenko <evgenii.shevchenko@fulldive.com>
2018-11-17 14:21:13 +0300

    Wrap longer lines

--- lib/main.dart
+++ lib/main.dart
@@ -138,15 +138,17 @@ class ChatMessage extends StatelessWidget {
             Container(
                 margin: const EdgeInsets.only(right: 16.0),
                 child: CircleAvatar(child: Text(_name[0]))),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(_name, style: Theme.of(context).textTheme.subhead),
-                Container(
-                  margin: const EdgeInsets.only(top: 5.0),
-                  child: Text(text),
-                )
-              ],
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(_name, style: Theme.of(context).textTheme.subhead),
+                  Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: Text(text),
+                  )
+                ],
+              ),
             )
           ],
         ),
